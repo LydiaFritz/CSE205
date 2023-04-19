0475705a72b1a1292916b6d46cac73d6f42ad5fb
Lydia <lydiakfritz@gmail.com>
2023-04-19 08:19:58 -0700

    added merge sort

--- src/sortAlgorithms/Sorts_Driver.java
+++ src/sortAlgorithms/Sorts_Driver.java
@@ -3,36 +3,38 @@ package sortAlgorithms;
 public class Sorts_Driver {
 	public static void main(String[] args) {
 
-		Integer[] foo = { 33,22,55,77,6,99,0,101,32,9,-7,55 };
+		String[] foo = { "a", "i", "e", "o", "y", "u" };
 
 		printList(foo);
 		System.out.println();
 
-		sortC(foo);
+		sort(foo);
 
 		printList(foo);
 		System.out.println();
-		
-		int loc = binSearch(foo, 101);
-		if(loc == -1)System.out.println(101 + " not found.");
-		else System.out.println(101 + " found at index " + loc);
+
+		/*
+		 * int loc = binSearch(foo, 101); if(loc == -1)System.out.println(101 +
+		 * " not found."); else System.out.println(101 + " found at index " + loc);
+		 */
 
 	}
-	
+
 	public static <T extends Comparable<T>> int binSearch(T[] arr, T key) {
 		int start = 0, last = arr.length;
-		int mid = (start+last)/2;
-		while((start < last) && !arr[mid].equals(key)) {
-			if(arr[mid].compareTo(key)<0) {
+		int mid = (start + last) / 2;
+		while ((start < last) && !arr[mid].equals(key)) {
+			if (arr[mid].compareTo(key) < 0) {
 				start = mid + 1;
-			}
-			else {
+			} else {
 				last = mid;
 			}
-			mid = (start+last)/2;
+			mid = (start + last) / 2;
 		}
-		if(arr[mid].equals(key))return mid;
-		else return -1;
+		if (arr[mid].equals(key))
+			return mid;
+		else
+			return -1;
 	}
 
 	public static <T extends Comparable<T>> int seqSearch(T[] arr, T key) {
@@ -43,7 +45,6 @@ public class Sorts_Driver {
 	}
 
 	public static <T extends Comparable<T>> void sortC(T[] arr) {
-
 		T temp = null;
 
 		for (int i = 0; i < arr.length; i++) {
@@ -57,8 +58,66 @@ public class Sorts_Driver {
 		}
 	}
 
-	public static <T extends Comparable<T>> void sortB(T[] arr) {
+	public static void sort(String[] a) {
+		if (a.length <= 1) {
+			return;
+		}
+		String[] first = new String[a.length / 2];
+		String[] second = new String[a.length - first.length];
+		// Copy the first half of a into first, the second half into second
+		for (int i = 0; i < first.length; i++) {
+			first[i] = a[i];
+		}
+		for (int i = 0; i < second.length; i++) {
+			second[i] = a[first.length + i];
+		}
+		sort(first);
+		sort(second);
+		merge(first, second, a);
+	}
 
+	private static void merge(String[] arr1, String[] arr2, String[] result) {
+		// Merge both halves into a sorted array
+		int i = 0, j = 0, k = 0;
+		while (i < arr1.length && j < arr2.length) {
+			if (arr1[i].compareTo(arr2[j]) < 0) {
+				// arr[i] is smaller
+				result[k++] = arr1[i++];
+			} else {
+				result[k++] = arr2[j++];
+			}
+		}
+		// see if there are any elements left between start and middle
+		while (i != arr1.length) {
+			result[k++] = arr1[i++];
+		}
+		while (j != arr2.length) {
+			result[k++] = arr2[j++];
+		}
+
+	}
+
+	public static <T extends Comparable<T>> void sortB(T[] arr) {
+		int lastSorted = arr.length;
+		for (int i = 0; i < lastSorted; i++) {
+			// find the max
+			int maxIndex = findMax(arr, 0, lastSorted);
+			// swap with last sorted
+			lastSorted--;
+			T temp = arr[lastSorted];
+			arr[lastSorted] = arr[maxIndex];
+			arr[maxIndex] = temp;
+		}
+	}
+
+	private static <T extends Comparable<T>> int findMax(T[] arr, int first, int last) {
+		// find the max value between index first (inclusive) and index last(exclusive)
+		int maxIndex = first;
+		for (int i = first + 1; i < last; i++) {
+			if (arr[maxIndex].compareTo(arr[i]) < 0)
+				maxIndex = i;
+		}
+		return maxIndex;
 	}
 
 	public static <T extends Comparable<T>> void sortA(T[] arr) {
